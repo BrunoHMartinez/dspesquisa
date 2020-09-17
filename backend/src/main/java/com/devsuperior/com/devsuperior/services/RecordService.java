edc7beedb71899c6d8617db907bd577e3df9c4fe
BrunoHMartinez <bruno.h_m@hotmail.com>
2020-09-17 16:21:24 -0300

    GET records

--- backend/src/main/java/com/devsuperior/com/devsuperior/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/com/devsuperior/services/RecordService.java
@@ -3,6 +3,8 @@ package com.devsuperior.com.devsuperior.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +24,7 @@ public class RecordService {
 	@Autowired
 	private GameRepository gameRepository;
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public RecordDTO insert(RecordInsertDTO dto) {
 		Record entity = new Record();
 
@@ -35,5 +37,11 @@ public class RecordService {
 
 		entity = repository.save(entity);
 		return new RecordDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 }
